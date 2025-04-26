93582288e222c1f99afb3a624adee18b8fd0d620
karaykaray77 <karaykaray77@gmail.com>
2025-04-26 19:05:46 +0300

    degisiklik

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
